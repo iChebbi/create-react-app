a74dd54402f376092b21087eea29d568b246ca10
Chebbi Aymen <aymen.chebi@gmail.com>
2019-01-29 12:16:35 +0100

    Bug fixes

--- packages/react-scripts/template/src/components/home/Home.jsx
+++ packages/react-scripts/template/src/components/home/Home.jsx
@@ -21,6 +21,7 @@ export default class Home extends Component {
           <p>
             Edit
             <code>src/App.js</code>
+            {' '}
             and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
